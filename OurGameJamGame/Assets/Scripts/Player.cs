406bb6810cccab5e06d4b094cf3c547086d4fc28
goADX <a.s.alaydaroos@gmail.com>
2026-01-30 01:33:19 +0800

    some work in player movement script

--- OurGameJamGame/Assets/Scripts/Player.cs
+++ OurGameJamGame/Assets/Scripts/Player.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Velocity velocity;
+    public float jumpForce;
+    public LayerMask groundLayer;
+    public Rigidbody2D rb;
+    private bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,37 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space)|| Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+        }
+        if(Physics2D.OverlapCircle(transform.position, 0.1f, groundLayer))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        if (isGrounded)
+        {
+            if(Input.GetKeyDown(KeyCode.LeftArrow)|| Input.GetKeyDown(KeyCode.A))
+            {
+                rb.velocity = new Vector2(-velocity.speed, rb.velocity.y);
+            }
+            if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            {
+                rb.velocity = new Vector2(velocity.speed, rb.velocity.y);
+            }
+        }
     }
+
+    private void Jump()
+    {
+        if(isGrounded)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
 }
