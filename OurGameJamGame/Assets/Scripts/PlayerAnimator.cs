2c82821a43be3e434e784d2153f64f3a896e245b
goADX <a.s.alaydaroos@gmail.com>
2026-01-31 05:46:52 +0800

    made change MASK

--- OurGameJamGame/Assets/Scripts/PlayerAnimator.cs
+++ OurGameJamGame/Assets/Scripts/PlayerAnimator.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
-
-    public Sprite[] frames;
+    public Material material;
+    //idle is from 0 to 3
+    //walking from 4 to 7
+    //jumping is 8
+    //falling from 9 to 12
     public float framesPerSecondIdle = 4.0f;
     public float framesPerSecondRun = 10.0f;
     public float framesPerSecondAir = 12.0f;
     private SpriteRenderer spriteRenderer;
     private MaskManager maskManager;
+    int currentFrameIndex = 0;
     public masks maskType
     {
         get{ return maskManager.currentMask; }
@@ -24,8 +29,35 @@ public class PlayerAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(frames.Length == 0) return;
-        
+        switch (maskType)
+        {
+            case masks.none:
+                material.SetFloat("_Mask", 0);
+                break;
+            case masks.PlagueMask:
+                material.SetFloat("_Mask", 1);
+                break;
+            case masks.GoblinMask:
+                material.SetFloat("_Mask", 2);
+                break;
+            case masks.TimeBoyMask:
+                material.SetFloat("_Mask", 3);
+                //AnimateTimeBoy();
+                break;
+            default:
+                material.SetFloat("_Mask", 0);
+                break;
+        }
 
+
+
+
+    }
+
+
+
+    public void SetMaterialIndex(int index)
+    {
+        material.SetFloat("_State", index);
     }
 }
--- OurGameJamGame/Assets/Scripts/knightBigAbility.cs
+++ OurGameJamGame/Assets/Scripts/knightBigAbility.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class knightBigAbility : MonoBehaviour
 {
+    public float FirstStageTime = 2f;
+    public float SecondStageTime = 2f;
+
+    /*
+    we will first make big rectange, detect all enemies in the rectangle
+    at the same time we summon the first VFX 
+    and we will stay for the entire diration of the first stage without being able to move or walk
+    second stage summon a particale on each enemy detected and wait for the end of the second stage to end
+    then we deal damage to all enemies remove all vfx teleport to the furthest enemy 
+    */
     // Start is called before the first frame update
     void Start()
     {
